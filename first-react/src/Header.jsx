2806866c81d8db5dcd13f1949c3148c1133919ff
Adrian <caesar.adrian88@gmail.com>
2022-10-29 01:36:14 +0700

    v3.0

--- first-react/src/Header.jsx
+++ first-react/src/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import Logo from "./assets/react-logo.png"
 
 export default function Header() {
     return (
         <header>
             <nav className="nav-bar">
-                <img className="nav-img" src="./src/react-logo.png" />
+                <img className="nav-img" src={Logo} />
                 <ul className="nav-items">
                     <li>Pricing</li>
                     <li>About</li>
