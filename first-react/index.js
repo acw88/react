c48771fd2d7211f9238a1cd95e119f3588b9d905
Adrian <caesar.adrian88@gmail.com>
2022-10-28 23:54:09 +0700

    v1.2

--- first-react/index.js
+++ first-react/index.js
@@ -1,61 +1,17 @@
-function Page() {
-	return (
-		<div>
-			<header>
-				<nav>
-					<img src="react-logo.png" width="40" />
-				</nav>
-			</header>
-			<h1>Fun facts about React</h1>
-			<ol>
-				<li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-            	<li>I'm more likely to get a job as a developer if I know React</li>
-	 		</ol>
-	 		<footer>
-	 			<small>© 20xx acw development. All rights reserved.</small>
-	 		</footer>
-		</div>
-	);
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"));
-
-// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
-// function MainContent() {
-// 	return (
-// 		<h1>Hello, everyone</h1>
-// 	)
-// }
-
-// ReactDOM.render(
-// 	<MainContent />, 
-// 	document.getElementById("root"));
-
-// const element = <h1 className="header">This is JSX</h1>;
-// console.log(element);
-
-// const page = (
-// 	<div>
-//         <h1 className="header">This is JSX</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// );
-
-// const navbar = (
-// 	<nav>
-// 		<h1>My Website</h1>
-// 		<ul>
-// 			<li>Pricing</li>
-// 			<li>About</li>
-// 			<li>Contact</li>
-// 		</ul>
-// 	</nav>
-// )
-
-// ReactDOM.render(
-// 	navbar, 
-//     document.getElementById("root"));
-
-// ReactDOM.createRoot(document.getElementById("root")).render(navbar);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
